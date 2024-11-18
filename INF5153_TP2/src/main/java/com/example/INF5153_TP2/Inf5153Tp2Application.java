b6e3185838c7aadd7c01bb37a3d2855a069e46ca
Juan Carlos <meridacortesj@gmaiil.com>
2024-11-18 13:11:53 -0500

    "hello test to check if server runs correctly"

--- INF5153_TP2/src/main/java/com/example/INF5153_TP2/Inf5153Tp2Application.java
+++ INF5153_TP2/src/main/java/com/example/INF5153_TP2/Inf5153Tp2Application.java
@@ -2,12 +2,21 @@ package com.example.INF5153_TP2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Inf5153Tp2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Inf5153Tp2Application.class, args);
 	}
 
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello", "World");
+	}
 }
