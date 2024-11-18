175176fbde2e954f425ddc80f0aa5cfba0f08b24
Juan Carlos <meridacortesj@gmaiil.com>
2024-11-18 12:40:04 -0500

    "Added initial config for the project"

---
+++ INF5153_TP2/src/main/java/com/example/INF5153_TP2/Inf5153Tp2Application.java
@@ -0,0 +1,13 @@
+package com.example.INF5153_TP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inf5153Tp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Inf5153Tp2Application.class, args);
+	}
+
+}
---
+++ INF5153_TP2/src/test/java/com/example/INF5153_TP2/Inf5153Tp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.INF5153_TP2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inf5153Tp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
